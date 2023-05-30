8c518ae6e152b87934208144c82a6f52bf9929b0
ptdo <ptdo@sfu.ca>
2023-05-30 12:06:08 -0700

    fixes for build errors

--- src/components/WeeklyLayout.js
+++ src/components/WeeklyLayout.js
@@ -5,8 +5,8 @@ export default function WeeklyLayout({ weeklyData, unit }) {
       <>
         <h1 className='text-3xl font-sans font-bold text-gray-800 mb-8 mt-8'>WEEKLY FORECAST</h1>
         <div className="grid grid-cols-1 gap-3 md:grid-cols-7 md:gap-4">
-            {weeklyData.list?.map((data) => 
-              <WeeklyCard data={data} unit={unit} />
+            {weeklyData.list?.map((data, index) => 
+              <WeeklyCard key={index} data={data} unit={unit} />
             )}
         </div>
       </>
